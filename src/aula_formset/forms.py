8983e2a9d9451cc294d459d744d6c140b0c2236b
Edgar-Bruno <edgar.brunos@gmail.com>
2015-10-27 22:23:11 -0200

    Criado teste do campo form.Select

--- src/aula_formset/forms.py
+++ src/aula_formset/forms.py
@@ -9,13 +9,23 @@ class NaveForms(forms.ModelForm):
 		fields = ['navenome']
 
 class Ficha_tForms(forms.ModelForm):
+
+    var = ['Edgar Bruno', 'Vivi Nicolao', 'Pedro N. Bruno', 'Sanjiro', 'Marmota']
+
+    radio = forms.ChoiceField(choices=[(index, valor) for index, valor in enumerate(var)])
+
     class Meta:
         model = Ficha_t
         fields = ['caracteristica', 'informacao', 'radio']
         exclude = ('nave',)
 
-    CHOICES = (('1', 'Um',), ('2', 'Dois',))
-    radio = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES,
-    						help_text = ("Enter the same password as above, for verification."))
+        widgets = {
+            'radio' : forms.Select(attrs={'class':'select'}),
+        }
+        
+
+    #CHOICES = (('1', 'Um',), ('2', 'Dois',))
+    #radio = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES,
+    #						help_text = ("Enter the same password as above, for verification."))
 
 Ficha_tFormset = formsets.formset_factory(Ficha_tForms)
